45c8f05afc7c28613f515bb08e4296ce20e2f13d
selma-lindfors <selma.lindfors@gmail.com>
2019-06-28 13:04:14 +0300

    make 'Play Again' button work

--- end.js
+++ end.js
@@ -3,6 +3,7 @@ const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 const saveScoreBtn = document.getElementById("saveScoreButton");
 const id =  window.location.search.split("=")[1];
+const playAgainButton = document.getElementById("again-button");
 
 const highscores = JSON.parse(localStorage.getItem("highscores" + id)) || [];
 const MAX_HIGHSCORES = 5;
@@ -10,6 +11,8 @@ const MAX_HIGHSCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup",() => saveScoreBtn.disabled = !username.value);
+
+playAgainButton.href = ("game.html?contentId=" + id);
 
 const saveHighscore = e => {
     e.preventDefault();
