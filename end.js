39df5cca7489570c25e81b63e349f42c2a14f5b1
selma-lindfors <selma.lindfors@gmail.com>
2019-07-02 13:24:56 +0300

    indentation fix

--- end.js
+++ end.js
@@ -2,28 +2,29 @@ const username = document.getElementById("username");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 const saveScoreBtn = document.getElementById("saveScoreButton");
-const id =  window.location.search.split("=")[1];
+const id = window.location.search.split("=")[1];
 const playAgainButton = document.getElementById("again-button");
 
 const highscores = JSON.parse(localStorage.getItem("highscores" + id)) || [];
 const MAX_HIGHSCORES = 5;
 
-
 finalScore.innerText = mostRecentScore;
-username.addEventListener("keyup",() => saveScoreBtn.disabled = !username.value);
+username.addEventListener(
+  "keyup",
+  () => (saveScoreBtn.disabled = !username.value)
+);
 
-playAgainButton.href = ("game.html?contentId=" + id);
+playAgainButton.href = "game.html?contentId=" + id;
 
 const saveHighscore = e => {
-    e.preventDefault();
-    const currentScore = {
-        score: mostRecentScore,
-        name: username.value
-    }
-    highscores.push(currentScore)
-    highscores.sort((x,y) => y.score-x.score);
-    highscores.splice(MAX_HIGHSCORES);
-    localStorage.setItem("highscores"+id, JSON.stringify(highscores));
-    window.location.assign("highscores.html?contentId="+id);
-} 
-
+  e.preventDefault();
+  const currentScore = {
+    score: mostRecentScore,
+    name: username.value
+  };
+  highscores.push(currentScore);
+  highscores.sort((x, y) => y.score - x.score);
+  highscores.splice(MAX_HIGHSCORES);
+  localStorage.setItem("highscores" + id, JSON.stringify(highscores));
+  window.location.assign("highscores.html?contentId=" + id);
+};
--- highscores.js
+++ highscores.js
@@ -1,13 +1,25 @@
 const highscoreList = document.getElementById("highscoreList");
-const id =  window.location.search.split("=")[1];
-const highscores = JSON.parse(localStorage.getItem("highscores"+id)) || [];
+const id = window.location.search.split("=")[1];
+const highscores = JSON.parse(localStorage.getItem("highscores" + id)) || [];
 let contentName = document.getElementById("contentName");
 
-    highscoreList.innerHTML = highscores.map(score =>
-    `<tr>
-    <th class="highscoreNames">${score.name.substring(0,16)}</th><th class="highscoreScores">${score.score}</th></tr>`).join(""); 
- 
-    if(id === "1"){contentName.innerText = "Crash Course Computer Science"}
-    if(id === "2"){contentName.innerText = "JavaScript"}
-    if(id === "0"){contentName.innerText = "All Content"}
+highscoreList.innerHTML = highscores
+  .map(
+    score =>
+      `<tr>
+    <th class="highscoreNames">${score.name.substring(
+      0,
+      16
+    )}</th><th class="highscoreScores">${score.score}</th></tr>`
+  )
+  .join("");
 
+if (id === "1") {
+  contentName.innerText = "Crash Course Computer Science";
+}
+if (id === "2") {
+  contentName.innerText = "JavaScript";
+}
+if (id === "0") {
+  contentName.innerText = "All Content";
+}
